df8ff802b2fbd69f8b286e90ebd044542a7f820d
emirhanipek <emirhan_ipek04@hotmail.com>
2025-03-10 14:40:37 +0300

    Testler Düzenlendi

--- call_page.py
+++ call_page.py
@@ -1,7 +1,9 @@
-from utils import click_element
+from utils import click_element, scrool_bar
 from selenium.webdriver.common.by import By  
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
 import time
 def call_page(driver):
     driver.find_element(By.XPATH , '//*[@id="app"]/div/nav/nav/div/div/div[1]/div[2]/div/div/div[1]').click()
@@ -31,7 +33,8 @@ def call_page(driver):
 
     name = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[2]/div/form/div[1]/input')
     name.click()
-    name.send_keys("emir")
+    today_date = datetime.datetime.now().strftime("%d/%m/%Y")
+    name.send_keys(f"emir {today_date}")
     time.sleep(1)
 
     driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[2]/div/form/div[2]/div/div[2]').click()
@@ -65,6 +68,93 @@ def call_page(driver):
     driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[2]/div/form/div[7]/button[2]').click()
 
     click_element(driver,By.CLASS_NAME,"swal2-styled","call_page_ok_button")
+
+    # konuşmaya tıkla
+    div_xpath = f"//div[contains(text(), 'emir {today_date}')]"
+    div_element = driver.find_element(By.XPATH, div_xpath)
+    div_element.click()
+
+    #follow up mesajıdır.
+    followup_button = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div[2]/button[1]')
+    followup_button.click()
+
+    message_add = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/div/div/button[2]')
+    message_add.click()
+
+    status_add = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr/td[1]/div/div[2]')
+    status_add.click()
+
+    status_add_element = driver.find_element(By.XPATH , '//*[@id="null-2"]')
+    status_add_element.click()  
+
+    message_text = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr/td[2]/div/div[2]')
+    message_text.send_keys("Bu bir follow-up mesajıdır")  
+
+    driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr/td[3]/button[1]').click()
+
+    message_add.click()
+
+    status_add = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr/td[1]/div/div[2]')
+    status_add.click()
+
+    status_add_element = driver.find_element(By.XPATH , '//*[@id="null-1"]')
+    status_add_element.click()  
+
+    message_text = driver.find_element(By.XPATH , 'Bu bir follow-up mesajıdır')
+
+    driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr[2]/td[3]/button[2]').click()
+
+    driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/table/tbody/tr/td[3]/button[2]')
+
+    ok_button = WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, "swal2-confirm")))
+    ok_button.click()
+
+    yapay_zeka_oluştur = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[2]/div/div/div/button[1]')
+    yapay_zeka_oluştur.click()
+
+    time.sleep(50)
+
+    close_followup = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div/div[3]/div[2]/button[2]')
+    close_followup.click()
+
+    #sohbet mesajı oluştur
+    chat_add = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/div[2]/button[2]')
+    chat_add.click()
+
+    yapay_zeka_button = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div/div/div[2]/div/div/div/button[1]')
+    yapay_zeka_button.click()
+
+    konu_message = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div[1]/div/form/div[1]/textarea')
+    konu_message.send_keys("Müşterimize abonelik yenileme teklifinde bulunacağız. Geçmiş faturalar ve kullanım analizleri doğrultusunda kişiye özel kampanyalar sunarak ikna etmeye çalışacağız.")
+
+    driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div[1]/div/form/div[2]/button[2]').click()
+    time.sleep(15)
+
+    success = WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, "swal2-confirm")))
+    success.click()
+
+    scrool_bar(driver)
+
+    chat_follow_close = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div/div/div[1]/button')
+    chat_follow_close.click()
+
+    #customer add
+
+    customer_add = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[4]/button[2]')
+    customer_add.click()
+
+    customer_select = driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div/form/div[1]/div/div[2]')
+    customer_select.send_keys("emir ipek")
+    customer_select.send_keys(Keys.RETURN)
+
+    customer_info = driver.find_element(By.XPATH , '//*[@id="note"]')
+    customer_info.send_keys("Müşterimizin abonelik süresi dolmak üzere. Müşterimizle iletişime geçerek abonelik süresini uzatmaya çalışacağız. ")
+    
+    driver.find_element(By.XPATH , '//*[@id="app"]/div/main/div/div[3]/div/form/div[3]/button[2]').click()
+
+    customer_add_success = WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, "swal2-confirm")))
+    customer_add_success.click()
+
     print("Giden Arama Sayfası Görüntülendi")
     time.sleep(2)
 
